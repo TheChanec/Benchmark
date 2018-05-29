93d39fa892c582b4117e7c6c6c2167091929cdc8
Enrique Chan Fernandez <enrique.nec@gmail.com>
2018-05-29 10:20:06 -0500

    Objet

---
+++ Library_benchmark/Models/Elemento1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Library_benchmark.Models
+{
+    public class Elemento1
+    {
+        public string Propiedad1 { get; set; }
+        public string Propiedad2 { get; set; }
+        public string Propiedad3 { get; set; }
+        public string Propiedad4 { get; set; }
+        public string Propiedad5 { get; set; }
+        public string Propiedad6 { get; set; }
+        public string Propiedad7 { get; set; }
+        public string Propiedad8 { get; set; }
+        public string Propiedad9 { get; set; }
+        public string Propiedad10 { get; set; }
+        public string Propiedad11 { get; set; }
+        public string Propiedad12 { get; set; }
+        public string Propiedad13 { get; set; }
+        public string Propiedad14 { get; set; }
+        public string Propiedad15 { get; set; }
+        public string Propiedad16 { get; set; }
+        public string Propiedad17 { get; set; }
+        public string Propiedad18 { get; set; }
+        public string Propiedad19 { get; set; }
+        public string Propiedad20 { get; set; }
+        public string Propiedad21 { get; set; }
+        public string Propiedad22 { get; set; }
+        public string Propiedad23 { get; set; }
+        public string Propiedad24 { get; set; }
+        public string Propiedad25 { get; set; }
+        public string Propiedad26 { get; set; }
+
+    }
+}
