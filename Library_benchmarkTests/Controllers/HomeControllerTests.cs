45b7794f9bc0e7545c4f18a604f0703c0e3c8da0
Enrique Chan Fernandez <enrique.nec@gmail.com>
2018-05-29 10:21:13 -0500

    Controller extra

---
+++ Library_benchmarkTests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Library_benchmark.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Library_benchmark.Controllers.Tests
+{
+    [TestClass()]
+    public class HomeControllerTests
+    {
+        [TestMethod()]
+        public void EPPLUSResultTest()
+        {
+            HomeController controller = new HomeController();
+            var parametros = new Models.Parametros() { Cols =5 , Rows = 3  } ;
+            //var result = controller.EPPLUSResult(parametros);
+
+            Assert.Fail();
+        }
+    }
+}
