688d54ec52cccba98ea4c5e0a4f36583f0f741e9
Enrique Chan Fernandez <enrique.nec@gmail.com>
2018-06-12 12:01:48 -0500

    Using aptimization

--- Library_benchmarkTests/Controllers/HomeControllerTests.cs
+++ Library_benchmarkTests/Controllers/HomeControllerTests.cs
@@ -1,10 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Library_benchmark.Controllers;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace Library_benchmark.Controllers.Tests
 {
