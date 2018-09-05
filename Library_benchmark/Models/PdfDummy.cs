4e0adb6b1aa7013a346917fef08005a236752a4a
Enrique Chan Fernandez <enrique.nec@gmail.com>
2018-09-05 10:09:23 -0500

    Cambio a string en entidad

--- Library_benchmark/Models/PdfDummy.cs
+++ Library_benchmark/Models/PdfDummy.cs
@@ -6,12 +6,12 @@ namespace Library_benchmark.Models
     {
         public string Title { get; set; }
         public string SubTitle { get; set; }
-        public DateTime Date { get; set; }
+        public string Date { get; set; }
         public string Driver { get; set; }
         public string Truck { get; set; }
-        public int OdometerStatr { get; set; }
-        public int PressurePsi { get; set; }
-        public int DevicePsi { get; set; }
+        public string OdometerStatr { get; set; }
+        public string PressurePsi { get; set; }
+        public string DevicePsi { get; set; }
         public string Water { get; set; }
         public string MechanicalComments { get; set; }
 
